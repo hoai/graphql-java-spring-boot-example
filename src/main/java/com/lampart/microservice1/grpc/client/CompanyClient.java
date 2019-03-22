e82608beb52a60f0b2ebb3767ada43e031314dbe
[Lampart] Nguyen Van Hoai <van_hoai@lampart-vn.com>
2019-03-22 13:25:54 +0700

    fix bug 3

--- src/main/java/com/lampart/microservice1/grpc/client/CompanyClient.java
+++ src/main/java/com/lampart/microservice1/grpc/client/CompanyClient.java
@@ -24,7 +24,7 @@ public class CompanyClient {
 //    private DiscoveryClient discoveryClient;
 // 
 //    private LoadBalancerClient loadBalancer;
-    
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(CompanyClient.class);
 
 //	public CompanyClient(DiscoveryClient discoveryClient, LoadBalancerClient loadBalancer) {
@@ -32,65 +32,59 @@ public class CompanyClient {
 //		this.loadBalancer = loadBalancer;
 //	}
 
-	public static  CompanyResponse init(Integer Id, DiscoveryClient discoveryClient, LoadBalancerClient loadBalancer) throws InterruptedException {
-		
+	public static CompanyResponse init(Integer Id, DiscoveryClient discoveryClient, LoadBalancerClient loadBalancer)
+			throws InterruptedException {
+
 		String serviceName = "MICROSERVICE1".toLowerCase();
 		System.out.println(discoveryClient.getServices());
-//		List<ServiceInstance> instances = discoveryClient.getInstances(serviceName);
-//		if (instances == null || instances.isEmpty()) {
-//			throw new GraphQLException("No instances for service: " + serviceName); 
-//        }
-//		String html = "<h2>Instances for Service Id: " + serviceName + "</h2>";
-//		 
-//        for (ServiceInstance serviceInstance : instances) {
-//            html += "<h3>Instance :" + serviceInstance.getUri() + "</h3>";
-//            System.out.println(serviceInstance.getUri());
-//        }
-// 
-//        html += "<br><h4>Call /hello of service: " + serviceName + "</h4>";
-        
-//        try {
-            // May be throw IllegalStateException (No instances available)
-            ServiceInstance serviceInstance = loadBalancer.choose(serviceName);
- 
-            //html += "<br>===> Load Balancer choose: " + serviceInstance.getUri();
- 
-            String url = "choose host: http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/";
-            System.out.println(url);
- 
-         // Channel is the abstraction to connect to a service endpoint
-    		// Let's use plaintext communication because we don't have certs
-    		final ManagedChannel channel = ManagedChannelBuilder.forAddress(serviceInstance.getHost(), serviceInstance.getPort()).usePlaintext(true).build();
-    		//final ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 4002).usePlaintext(true).build();
+		List<ServiceInstance> instances = discoveryClient.getInstances(serviceName);
+		if (instances == null || instances.isEmpty()) {
+			throw new GraphQLException("No instances for service: " + serviceName);
+		}
 
-    		// It is up to the client to determine whether to block the call
-    		// Here we create a blocking stub, but an async stub,
-    		// or an async stub with Future are always possible.
-    		CompanyServiceGrpc.CompanyServiceBlockingStub stub = CompanyServiceGrpc.newBlockingStub(channel);
-    		CompanyServiceOuterClass.CompanyRequest request = CompanyServiceOuterClass.CompanyRequest.newBuilder().setId(Id).build();
+		for (ServiceInstance serviceInstance : instances) {
+			System.out.println(serviceInstance.getUri());
+		}
 
-    		// Finally, make the call using the stub
-    		CompanyServiceOuterClass.CompanyResponse response = stub.getCompany(request);
+		try {
+			// May be throw IllegalStateException (No instances available)
+			ServiceInstance serviceInstance = loadBalancer.choose(serviceName);
+			System.out.println("Load Balancer choose: " + serviceInstance.getUri());
+			// html += "<br>===> Load Balancer choose: " + serviceInstance.getUri();
 
-    		System.out.println(response);
+//            String url = "choose host: http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/";
+//            System.out.println(url);
 
-    		// A Channel should be shutdown before stopping the process.
-    		channel.shutdownNow();
-    		return response;
-            
-//        } catch (IllegalStateException e) {
-//            //html += "<br>loadBalancer.choose ERROR: " + e.getMessage();
-//            e.printStackTrace();
-//        } catch (Exception e) {
-//            //html += "<br>Other ERROR: " + e.getMessage();
-//            e.printStackTrace();
-//        }
-//        
-//        return null;
-        
-		
+			// Channel is the abstraction to connect to a service endpoint
+			// Let's use plaintext communication because we don't have certs
+			final ManagedChannel channel = ManagedChannelBuilder
+					.forAddress(serviceInstance.getHost(), serviceInstance.getPort()).usePlaintext(true).build();
 
-		
+			// It is up to the client to determine whether to block the call
+			// Here we create a blocking stub, but an async stub,
+			// or an async stub with Future are always possible.
+			CompanyServiceGrpc.CompanyServiceBlockingStub stub = CompanyServiceGrpc.newBlockingStub(channel);
+			CompanyServiceOuterClass.CompanyRequest request = CompanyServiceOuterClass.CompanyRequest.newBuilder()
+					.setId(Id).build();
+
+			// Finally, make the call using the stub
+			CompanyServiceOuterClass.CompanyResponse response = stub.getCompany(request);
+
+			System.out.println(response);
+
+			// A Channel should be shutdown before stopping the process.
+			channel.shutdownNow();
+			return response;
+
+		} catch (IllegalStateException e) {
+			System.out.println("loadBalancer.choose ERROR: " + e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Other ERROR: " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		return null;
 
 	}
 }
